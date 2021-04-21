7630f203d92842ee8e2163de839d34187062e841
Darius Atlas <mit.3tlasa@gmail.com>
2021-04-20 20:58:54 -0700

    set nan data for element

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+const gloAcademyList = document.querySelector('.glo-academy-list');
+
+
+const createList = (wrapper, listCard) => {
+    wrapper.textContent = '';
+};
+createList(gloAcademyList);
